1f44b2fc697809f2404b955643f8f57b85d27503
lpakingan <lpak98@gmail.com>
2023-07-09 18:56:35 -0700

    fix pathing issues with handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -64,4 +64,15 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+// redirect to the signup page if user wants to sign up
+router.get("/signup", (req, res) => {
+  // if a session exists, redirect the user to the homepage; else redirect to login screen
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
 module.exports = router;
